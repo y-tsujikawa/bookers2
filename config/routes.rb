78de9770b55d6c41dce0c69d6e25059afc7ea655
y-tsujikawa <184855547+y-tsujikawa@users.noreply.github.com>
2024-11-15 20:25:57 +0000

    [Add] ルーティングの記述変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
   root to: "homes#top"
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :books, only: [:new, :index, :show, :edit]
+  resources :users, only: [:show, :edit]
 end
