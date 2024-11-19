7e48e22697a2ef37e6b5a1f772ba98c89e4da192
y-tsujikawa <184855547+y-tsujikawa@users.noreply.github.com>
2024-11-19 07:01:16 +0000

    [Add] 新規投稿が成功したとき

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+      flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book)
     else
       render :new
